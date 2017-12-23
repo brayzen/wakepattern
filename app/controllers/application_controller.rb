6ae3de054825ad7669b52ded29eb850b21e3961f
soccerkevin <soccerkevint11@gmail.com>
2017-12-23 01:04:11 -0800

    refactor guest user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,24 @@ class ApplicationController < ActionController::Base
 
   # if user is logged in, return current_user, else return guest_user
   def current_or_guest_user
-    if current_user
-      if session[:guest_user_id] && session[:guest_user_id] != current_user.id
-        logging_in
-        # reload guest_user to prevent caching problems before destruction
-        guest_user(with_retry = false).try(:reload).try(:destroy)
-        session[:guest_user_id] = nil
-      end
-      current_user
-    else
-      guest_user
+    return guest_user unless current_user
+    if current_and_guest_user?
+      add_guest_to_current_user
+      delete_guest
     end
+    current_user
+  end
+
+  def delete_guest
+    # reload guest_user to prevent caching problems before destruction
+    guest_user(with_retry = false).try(:reload).try(:destroy)
+    session[:guest_user_id] = nil
+  end
+
+  def current_and_guest_user?
+    current_user &&
+    session[:guest_user_id] &&
+    session[:guest_user_id] != current_user.id
   end
 
   # find guest_user object associated with the current session,
@@ -31,7 +38,7 @@ class ApplicationController < ActionController::Base
 
   # called (once) when the user logs in, insert any code your application needs
   # to hand off from guest_user to current_user.
-  def logging_in
+  def add_guest_to_current_user
     # For example:
     # guest_comments = guest_user.comments.all
     # guest_comments.each do |comment|
@@ -41,9 +48,9 @@ class ApplicationController < ActionController::Base
   end
 
   def create_guest_user
-    u = User.new(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com")
-    u.save!(:validate => false)
-    session[:guest_user_id] = u.id
-    u
+    user = User.new name: 'guest', email: "guest_#{Time.now.to_i}#{rand(100)}@example.com"
+    user.save! validate: false
+    session[:guest_user_id] = user.id
+    user
   end
 end
