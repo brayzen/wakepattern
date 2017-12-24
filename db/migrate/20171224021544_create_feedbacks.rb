84a89117cae15e66b0bc7a1047bd1e6ee416173a
brayzen <brian.ray.biz@gmail.com>
2017-12-24 14:58:18 -0800

    add feedback*

---
+++ db/migrate/20171224021544_create_feedbacks.rb
@@ -0,0 +1,20 @@
+class CreateFeedbacks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :feedbacks do |t|
+			t.text     :message
+      t.integer  :user_rating
+			t.boolean  :flagged, default: false
+			t.boolean  :anonymous, default: true
+			t.boolean  :read
+			t.string   :to_first_name
+			t.string   :to_last_name
+			t.string   :to_email
+
+      t.timestamps
+    end
+		
+		#add_reference :to, :user, foreign_key: true
+		#add_reference :from, :user, foreign_key: true
+		
+  end
+end
