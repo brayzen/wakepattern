42ba32d383df525d01446b55a6607b922b2eca3c
soccerkevin <soccerkevint11@gmail.com>
2018-01-19 19:26:47 -0800

    add trait for order and indentation

--- app/models/feedback_trait.rb
+++ app/models/feedback_trait.rb
@@ -1,3 +1,15 @@
 class FeedbackTrait < ApplicationRecord
 	belongs_to :feedback, inverse_of: :feedback_traits
+
+  def trait
+    @trait ||= Trait.find trait_id
+  end
+
+  def name
+    trait.name
+  end
+
+  def indent
+    trait.indent
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+traits = Trait.create([
+  { id: 1, name: 'helpful',        order: 1,  indent: 0 },
+  { id: 2, name: 'dependable',     order: 2,  indent: 1 },
+  { id: 3, name: 'productive',     order: 3,  indent: 1 },
+  { id: 4, name: 'communication',  order: 4,  indent: 0 },
+  { id: 5, name: 'listening',      order: 5,  indent: 1 },
+  { id: 6, name: 'speech',         order: 6,  indent: 1 },
+  { id: 7, name: 'understanding',  order: 7,  indent: 1 },
+  { id: 8, name: 'charisma',       order: 8,  indent: 0 },
+  { id: 9, name: 'friendliness',   order: 9,  indent: 1 },
+  { id: 10, name: 'composure',     order: 10, indent: 1 },
+  { id: 11, name: 'complainer',    order: 11, indent: 1 }
+])
