502dc88de4e7161522dc7117a3fa21a5fa67578e
soccerkevin <soccerkevint11@gmail.com>
2018-01-31 00:12:41 -0800

    html complete

--- app/javascript/components/Feedback.jsx
+++ app/javascript/components/Feedback.jsx
@@ -4,14 +4,13 @@ import FeedbackTrait from './FeedbackTrait'
 
 class Feedback extends React.Component{
   render(){
-    console.log(this.props)
     return <div className="feedback">
         <div className="recipient">{ this.props.receiver.name }</div>
         <div className="message">{ this.props.message }</div>
         <div className="traits">
           {
             this.props.feedback_traits.map( (trait, i) =>{
-              return <FeedbackTrait rating={ trait.rating } key={i}/>
+              return <FeedbackTrait {...trait} key={i}/>
             })
           }
         </div>
