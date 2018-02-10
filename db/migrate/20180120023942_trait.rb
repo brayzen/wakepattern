e595bf89b49b884a684b51bfa192cb6b420076e6
soccerkevin <soccerkevint11@gmail.com>
2018-02-09 22:01:50 -0800

    saving this silly schema

--- db/migrate/20180120023942_trait.rb
+++ db/migrate/20180120023942_trait.rb
@@ -5,11 +5,5 @@ class Trait < ActiveRecord::Migration[5.1]
       t.integer :order
       t.integer :indent
     end
-
-    # we had an extra table
-    drop_table :feedback_trait
-
-    add_reference :feedback_traits, :trait
-    remove_column :feedback_traits, :name
   end
 end
--- db/migrate/20180210055028_create_joins_table_feedback_traits.rb
+++ db/migrate/20180210055028_create_joins_table_feedback_traits.rb
@@ -1,8 +1,12 @@
-class CreateFeedbackTraits < ActiveRecord::Migration[5.1]
+class CreateJoinsTableFeedbackTraits < ActiveRecord::Migration[5.1]
   def change
     create_table :feedback_traits do |t|
       t.integer :feedback_id
       t.index :feedback_id
+
+      t.integer :trait_id
+      t.index :trait_id
+
       t.string :name
       t.integer :rating
     end
