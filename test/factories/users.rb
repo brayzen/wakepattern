4a945484679e87163299df0a035a5b93a5ba29e6
soccerkevin <soccerkevint11@gmail.com>
2018-03-17 15:06:18 -0700

    test feedbacks/new/:handle

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
+  f_name = Faker::Name.first_name
+  l_name = Faker::Name.last_name
   factory :user do
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
-    email Faker::Internet.safe_email
+    first_name f_name
+    last_name l_name
+    email Faker::Internet.safe_email "#{f_name} #{l_name}"
+    handle l_name
     password 'password'
     password_confirmation 'password'
   end
