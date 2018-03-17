e7012b610200b3285d4a065fd997d1b9f0447761
soccerkevin <soccerkevint11@gmail.com>
2018-03-17 15:05:33 -0700

    should have a feedbacks/new/:handle route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :feedbacks
   get "givefeedback/:handle", to: 'feedbacks#new'
+  get "feedbacks/new/:handle", to: 'feedbacks#new'
   get 'about', to: 'static#about'
   get 'mission', to: 'static#mission'
   get 'mailer', to: 'static#mail_it'
