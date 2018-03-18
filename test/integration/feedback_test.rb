156a032fb9552ae7206d5393c2ef17a91ca1d505
soccerkevin <soccerkevint11@gmail.com>
2018-03-17 22:09:42 -0700

    saving

--- test/integration/feedback_test.rb
+++ test/integration/feedback_test.rb
@@ -12,9 +12,20 @@ class FeedbackPagesTest < Capybara::Rails::TestCase
     assert page.has_text? receiver.name
   end
 
-  test 'Create Feedback' do
+  test 'Create Feedback from handle' do
     receiver = create :receiver
     visit "/feedbacks/new/#{receiver.handle}"
+    fill_in 'feedback_message', with: @message
+    click_on 'Let them know how you feel'
+    assert_equal Feedback.last.message, @message
+  end
+
+  test 'Create Feedback from email' do
+    sender = create :sender
+    receiver = create :receiver
+    login_as sender
+    visit "/feedbacks/new"
+    fill_in 'feedback_receiver_attributes_email', with: receiver.email
     fill_in 'feedback_message', with: @message
     click_on 'Let them know how you feel'
     assert_equal Feedback.last.message, @message
